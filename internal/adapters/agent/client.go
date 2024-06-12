fb1ea9aa5eedfcd3906034f3327016621b8f3cbf
Григорий Никониров <mrgbh007@gmail.com>
2024-06-12 21:13:59 +0700

    Генерация клиента и сервера

---
+++ internal/adapters/agent/client.go
@@ -0,0 +1,3 @@
+package agent
+
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --target internal/client -package client --clean ../../../open_api_specs/agent.yaml
---
+++ internal/controllers/apiserver/server.go
@@ -0,0 +1,3 @@
+package apiserver
+
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen --target internal/server -package server --clean ../../../open_api_specs/server.yaml
