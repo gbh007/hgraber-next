d52060193c774d78e38899245d82689eee525129
Григорий Никониров <mrgbh007@gmail.com>
2025-01-11 16:51:13 +0700

    Добавлена сортировка задач в API

--- internal/controllers/apiserver/api_task_results_get.go
+++ internal/controllers/apiserver/api_task_results_get.go
@@ -2,6 +2,7 @@ package apiserver
 
 import (
 	"context"
+	"slices"
 
 	"hgnext/internal/entities"
 	"hgnext/internal/pkg"
@@ -17,28 +18,34 @@ func (c *Controller) APITaskResultsGet(ctx context.Context) (serverAPI.APITaskRe
 		}, nil
 	}
 
+	responseResults := pkg.Map(result, func(raw *entities.TaskResult) serverAPI.APITaskResultsGetOKResultsItem {
+		return serverAPI.APITaskResultsGetOKResultsItem{
+			Name:              raw.Name,
+			Error:             optString(raw.Error),
+			Result:            optString(raw.Result),
+			DurationFormatted: max(raw.Duration(), 0).String(),
+			StartedAt:         raw.StartedAt,
+			EndedAt:           raw.EndedAt,
+			Stages: pkg.Map(raw.Stages, func(rawStage *entities.TaskResultStage) serverAPI.APITaskResultsGetOKResultsItemStagesItem {
+				return serverAPI.APITaskResultsGetOKResultsItemStagesItem{
+					Name:              rawStage.Name,
+					Error:             optString(rawStage.Error),
+					Result:            optString(rawStage.Result),
+					DurationFormatted: max(rawStage.Duration(), 0).String(),
+					StartedAt:         rawStage.StartedAt,
+					EndedAt:           rawStage.EndedAt,
+					Progress:          rawStage.Progress,
+					Total:             rawStage.Total,
+				}
+			}),
+		}
+	})
+
+	slices.SortFunc(responseResults, func(a, b serverAPI.APITaskResultsGetOKResultsItem) int {
+		return b.StartedAt.Compare(a.StartedAt)
+	})
+
 	return &serverAPI.APITaskResultsGetOK{
-		Results: pkg.Map(result, func(raw *entities.TaskResult) serverAPI.APITaskResultsGetOKResultsItem {
-			return serverAPI.APITaskResultsGetOKResultsItem{
-				Name:              raw.Name,
-				Error:             optString(raw.Error),
-				Result:            optString(raw.Result),
-				DurationFormatted: max(raw.Duration(), 0).String(),
-				StartedAt:         raw.StartedAt,
-				EndedAt:           raw.EndedAt,
-				Stages: pkg.Map(raw.Stages, func(rawStage *entities.TaskResultStage) serverAPI.APITaskResultsGetOKResultsItemStagesItem {
-					return serverAPI.APITaskResultsGetOKResultsItemStagesItem{
-						Name:              rawStage.Name,
-						Error:             optString(rawStage.Error),
-						Result:            optString(rawStage.Result),
-						DurationFormatted: max(rawStage.Duration(), 0).String(),
-						StartedAt:         rawStage.StartedAt,
-						EndedAt:           rawStage.EndedAt,
-						Progress:          rawStage.Progress,
-						Total:             rawStage.Total,
-					}
-				}),
-			}
-		}),
+		Results: responseResults,
 	}, nil
 }
