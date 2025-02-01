e4d620ab7b9a5312b19281be7f19e9467d17198d
Григорий Никониров <mrgbh007@gmail.com>
2025-02-01 16:43:51 +0700

    Рефакторинг получения данных о системе на батч pgx

--- internal/adapters/postgresql/count.go
+++ internal/adapters/postgresql/count.go
@@ -5,102 +5,170 @@ import (
 	"database/sql"
 	"fmt"
 
+	"github.com/jackc/pgx/v5"
+
 	"hgnext/internal/entities"
 )
 
 func (d *Database) SystemSize(ctx context.Context) (entities.SystemSizeInfo, error) {
 	systemSize := entities.SystemSizeInfo{}
-
-	var err error
+	batch := &pgx.Batch{}
 
 	// Книги
 
-	err = d.db.GetContext(ctx, &systemSize.BookCount, `SELECT COUNT(*) FROM books;`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get book count : %w", err)
-	}
+	batch.Queue(`SELECT COUNT(*) FROM books;`).QueryRow(func(row pgx.Row) error {
+		err := row.Scan(&systemSize.BookCount)
+		if err != nil {
+			return fmt.Errorf("get book count : %w", err)
+		}
 
-	err = d.db.GetContext(ctx, &systemSize.DownloadedBookCount, `SELECT COUNT(*) FROM books WHERE deleted = FALSE AND page_count IS NOT NULL AND NOT EXISTS (SELECT 1 FROM pages WHERE book_id = books.id AND pages.downloaded = FALSE);`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get downloaded book count : %w", err)
-	}
+		return nil
+	})
 
-	err = d.db.GetContext(ctx, &systemSize.VerifiedBookCount, `SELECT COUNT(*) FROM books WHERE deleted = FALSE AND verified = TRUE AND page_count IS NOT NULL AND NOT EXISTS (SELECT 1 FROM pages WHERE book_id = books.id AND pages.downloaded = FALSE);`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get book verified count : %w", err)
-	}
+	batch.Queue(`SELECT COUNT(*) FROM books WHERE deleted = FALSE AND page_count IS NOT NULL AND NOT EXISTS (SELECT 1 FROM pages WHERE book_id = books.id AND pages.downloaded = FALSE);`).QueryRow(func(row pgx.Row) error {
+		err := row.Scan(&systemSize.DownloadedBookCount)
+		if err != nil {
+			return fmt.Errorf("get downloaded book count : %w", err)
+		}
 
-	err = d.db.GetContext(ctx, &systemSize.RebuildedBookCount, `SELECT COUNT(*) FROM books WHERE is_rebuild = TRUE;`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get book rebuilded count: %w", err)
-	}
+		return nil
+	})
 
-	err = d.db.GetContext(ctx, &systemSize.BookUnparsedCount, `SELECT COUNT(*) FROM books WHERE (name IS NULL OR page_count IS NULL OR attributes_parsed = FALSE) AND origin_url IS NOT NULL AND deleted = FALSE AND is_rebuild = FALSE;`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get book unparsed count: %w", err)
-	}
+	batch.Queue(`SELECT COUNT(*) FROM books WHERE deleted = FALSE AND verified = TRUE AND page_count IS NOT NULL AND NOT EXISTS (SELECT 1 FROM pages WHERE book_id = books.id AND pages.downloaded = FALSE);`).QueryRow(func(row pgx.Row) error {
+		err := row.Scan(&systemSize.VerifiedBookCount)
+		if err != nil {
+			return fmt.Errorf("get book verified count : %w", err)
+		}
 
-	err = d.db.GetContext(ctx, &systemSize.DeletedBookCount, `SELECT COUNT(*) FROM books WHERE deleted = TRUE;`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get book deleted count: %w", err)
-	}
+		return nil
+	})
+
+	batch.Queue(`SELECT COUNT(*) FROM books WHERE is_rebuild = TRUE;`).QueryRow(func(row pgx.Row) error {
+		err := row.Scan(&systemSize.RebuildedBookCount)
+		if err != nil {
+			return fmt.Errorf("get book rebuilded count: %w", err)
+		}
+
+		return nil
+	})
+
+	batch.Queue(`SELECT COUNT(*) FROM books WHERE (name IS NULL OR page_count IS NULL OR attributes_parsed = FALSE) AND origin_url IS NOT NULL AND deleted = FALSE AND is_rebuild = FALSE;`).QueryRow(func(row pgx.Row) error {
+		err := row.Scan(&systemSize.BookUnparsedCount)
+		if err != nil {
+			return fmt.Errorf("get book unparsed count: %w", err)
+		}
+
+		return nil
+	})
+
+	batch.Queue(`SELECT COUNT(*) FROM books WHERE deleted = TRUE;`).QueryRow(func(row pgx.Row) error {
+		err := row.Scan(&systemSize.DeletedBookCount)
+		if err != nil {
+			return fmt.Errorf("get book deleted count: %w", err)
+		}
+
+		return nil
+	})
 
 	// Страницы
 
-	err = d.db.GetContext(ctx, &systemSize.PageCount, `SELECT COUNT(*) FROM pages;`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get page count: %w", err)
-	}
+	batch.Queue(`SELECT COUNT(*) FROM pages;`).QueryRow(func(row pgx.Row) error {
+		err := row.Scan(&systemSize.PageCount)
+		if err != nil {
+			return fmt.Errorf("get page count: %w", err)
+		}
 
-	err = d.db.GetContext(ctx, &systemSize.PageUnloadedCount, `SELECT COUNT(*) FROM pages WHERE downloaded = FALSE;`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get unloaded page count: %w", err)
-	}
+		return nil
+	})
 
-	err = d.db.GetContext(ctx, &systemSize.PageWithoutBodyCount, `SELECT COUNT(*) FROM pages WHERE file_id IS NULL;`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get page without body count: %w", err)
-	}
+	batch.Queue(`SELECT COUNT(*) FROM pages WHERE downloaded = FALSE;`).QueryRow(func(row pgx.Row) error {
+		err := row.Scan(&systemSize.PageUnloadedCount)
+		if err != nil {
+			return fmt.Errorf("get unloaded page count: %w", err)
+		}
 
-	err = d.db.GetContext(ctx, &systemSize.DeletedPageCount, `SELECT COUNT(*) FROM deleted_pages;`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get deleted page count: %w", err)
-	}
+		return nil
+	})
+
+	batch.Queue(`SELECT COUNT(*) FROM pages WHERE file_id IS NULL;`).QueryRow(func(row pgx.Row) error {
+		err := row.Scan(&systemSize.PageWithoutBodyCount)
+		if err != nil {
+			return fmt.Errorf("get page without body count: %w", err)
+		}
+
+		return nil
+	})
+
+	batch.Queue(`SELECT COUNT(*) FROM deleted_pages;`).QueryRow(func(row pgx.Row) error {
+		err := row.Scan(&systemSize.DeletedPageCount)
+		if err != nil {
+			return fmt.Errorf("get deleted page count: %w", err)
+		}
+
+		return nil
+	})
 
 	// Файлы
 
-	err = d.db.GetContext(ctx, &systemSize.FileCount, `SELECT COUNT(*) FROM files;`)
+	batch.Queue(`SELECT COUNT(*) FROM files;`).QueryRow(func(row pgx.Row) error {
+		err := row.Scan(&systemSize.FileCount)
+		if err != nil {
+			return fmt.Errorf("get file count: %w", err)
+		}
+
+		return nil
+	})
+
+	batch.Queue(`SELECT COUNT(*) FROM files WHERE md5_sum IS NULL OR sha256_sum IS NULL OR "size" IS NULL;`).QueryRow(func(row pgx.Row) error {
+		err := row.Scan(&systemSize.UnhashedFileCount)
+		if err != nil {
+			return fmt.Errorf("get unhashed file count: %w", err)
+		}
+
+		return nil
+	})
+
+	batch.Queue(`SELECT COUNT(*) FROM dead_hashes;`).QueryRow(func(row pgx.Row) error {
+		err := row.Scan(&systemSize.DeadHashCount)
+		if err != nil {
+			return fmt.Errorf("get dead hash count: %w", err)
+		}
+
+		return nil
+	})
+
+	batch.Queue(`SELECT SUM(f."size") FROM pages AS p LEFT JOIN files AS f ON p.file_id = f.id WHERE f."size" IS NOT NULL;`).QueryRow(func(row pgx.Row) error {
+		size := sql.NullInt64{}
+
+		err := row.Scan(&size)
+		if err != nil {
+			return fmt.Errorf("get page file size: %w", err)
+		}
+
+		systemSize.PageFileSize = size.Int64
+
+		return nil
+	})
+
+	batch.Queue(`SELECT SUM("size") FROM files WHERE "size" IS NOT NULL;`).QueryRow(func(row pgx.Row) error {
+		size := sql.NullInt64{}
+
+		err := row.Scan(&size)
+		if err != nil {
+			return fmt.Errorf("get file size: %w", err)
+		}
+
+		systemSize.FileSize = size.Int64
+
+		return nil
+	})
+
+	batchResult := d.pool.SendBatch(ctx, batch)
+
+	err := batchResult.Close()
 	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get file count: %w", err)
+		return entities.SystemSizeInfo{}, err
 	}
-
-	err = d.db.GetContext(ctx, &systemSize.UnhashedFileCount, `SELECT COUNT(*) FROM files WHERE md5_sum IS NULL OR sha256_sum IS NULL OR "size" IS NULL;`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get unhashed file count: %w", err)
-	}
-
-	err = d.db.GetContext(ctx, &systemSize.DeadHashCount, `SELECT COUNT(*) FROM dead_hashes;`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get dead hash count: %w", err)
-	}
-
-	size := sql.NullInt64{}
-
-	err = d.db.GetContext(ctx, &size, `SELECT SUM(f."size") FROM pages AS p LEFT JOIN files AS f ON p.file_id = f.id WHERE f."size" IS NOT NULL;`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get page file size: %w", err)
-	}
-
-	systemSize.PageFileSize = size.Int64
-
-	size = sql.NullInt64{}
-
-	err = d.db.GetContext(ctx, &size, `SELECT SUM("size") FROM files WHERE "size" IS NOT NULL;`)
-	if err != nil {
-		return entities.SystemSizeInfo{}, fmt.Errorf("get file size: %w", err)
-	}
-
-	systemSize.FileSize = size.Int64
 
 	return systemSize, nil
 }
